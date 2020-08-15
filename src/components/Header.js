a0172296ead9067640c1b05c3bb45b40fb7d4ab4
Artyom <artem.tsvirko@mail.ru>
2020-08-15 23:06:52 +0300

    :nail_care: Add cool logo. Make app header more responsive.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types'
 
 const Header = ({ leftContent, rightContent }) => {
   return (
-    <Box display="flex" my={2} boxShadow={8}>
-      <Box display="flex" flexGrow={1} m={2}>
-        {leftContent}
-      </Box>
-      <Box width={48} height={48} m={2}>
-        {rightContent}
-      </Box>
+    <Box display="flex" mb={2} boxShadow={8}>
+      {leftContent && (
+        <Box display="flex" flexGrow={1}>
+          {leftContent}
+        </Box>
+      )}
+      {rightContent && (
+        <Box display="flex" flexDirection="column" justifyContent="center">
+          {rightContent}
+        </Box>
+      )}
     </Box>
   )
 }
--- src/pages/task/index.js
+++ src/pages/task/index.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 import Tabs from '../../components/Tabs'
 import TransitionAlert from '../../components/TransitionAlert'
 import TodoInput from '../../components/TodoInput'
-import GithubLogo from '../../components/GithubLogo'
+import AppLogo from '../../components/AppLogo'
+import GitHubLogoLink from '../../components/GitHubLogoLink'
 
 const TaskPage = () => {
   return (
     <Container>
-      <Header
-        leftContent={
-          <Typography variant="h4">Welcome to Arti's todo app!</Typography>
-        }
-        rightContent={
-          <a href="https://github.com/lartiquel/react-todo-app">
-            <GithubLogo />
-          </a>
-        }
-      />
+      <Header leftContent={<AppLogo />} rightContent={<GitHubLogoLink />} />
       <TodoInput my={3} />
       <TransitionAlert my={2} content="Task deleted!" />
       <Tabs my={2} />
