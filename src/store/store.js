6f0cb8b8713b0105d9d63040bea07a504ee8505c
Artyom <artem.tsvirko@mail.ru>
2020-08-26 01:10:57 +0300

    :bookmark: Add todo saving/loading functionality to the app.

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import debounce from 'lodash.debounce'
 import RootReducer from './reducers/rootReducer'
 
 const composeEnhancers =
@@ -9,7 +10,44 @@ const composeEnhancers =
     : compose
 const enhancer = composeEnhancers(applyMiddleware())
 
+// load prev state function
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (error) {
+    return undefined
+  }
+}
+
+// save current state function
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (error) {
+    console.log('State is not serializable!')
+  }
+}
+
 // create store and apply compose emhancers
-const store = createStore(RootReducer, enhancer)
+const store = createStore(RootReducer, loadState(), enhancer)
+
+store.subscribe(
+  debounce(() => {
+    saveState({
+      task: {
+        ...store.getState().task,
+        lastDeletedTodoId: '',
+        todos: store
+          .getState()
+          .task.todos.filter((todo) => todo.isMarkedToBeDeleted === false) // don't save `marked to be deleted` todos
+      }
+    })
+  }, 1000)
+)
 
 export default store
